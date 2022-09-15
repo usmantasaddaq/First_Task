1a1c1c8382067fd5b9248e449701077256632e5e
usmantasaddaq <usmantasaddaq501@gmail.com>
2022-09-15 19:41:28 +0500

    dice roll game

---
+++ Untitled-3.js
@@ -0,0 +1,60 @@
+//p1 sum of positive number.
+// var array = [2,3,-4,-1,6];
+
+// let sum=0;
+// array.filter(e=>{
+//     if(e>0){
+//      return sum = sum + e
+//     }
+// })
+//  console.log(sum)
+
+// //p2 
+// Write a JavaScript program where the program takes a random integer between 1 to 10,
+// function rand(){
+//     for(let i = 1;i<=10;i++){
+//   return  Math.floor(Math.random(i) *10) 
+// }
+
+// }
+
+// let b = rand()
+// console.log(b)
+//p#3
+
+// function myFunction(x,y){
+
+//  if(x + y ==50|| x==50||y==50){
+//     return true
+//  }else {
+//     return false
+//  }
+// }
+// let b =myFunction(10,40)
+// console.log(b)
+
+//p#4 /Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50
+// let array = [1,3,40,29,2,7,0,6];
+// let arr=[]
+// arr=array.sort(myfunction)
+
+// function myfunction(a,b){
+//     return a-b
+// }
+// console.log(arr)
+
+//p#5
+
+
+
+// let st = "*";
+// for(let i=0;i<5;i++){
+
+//  console.log(st)
+//  st = st+"*"
+// }
+
+
+
+
+
